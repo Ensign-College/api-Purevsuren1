ed2ca4fa18a997fa54e8649e3f2da14c292f3880
Purevsuren1 <113395417+Purevsuren1@users.noreply.github.com>
2024-01-17 10:01:30 -0700

    data created

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello");
+const express = require("express")
+const app = express();
+ 
+const boxes = [
+    {boxId:1},
+    {boxId:2},
+    {boxId:3},
+    {boxId:4},
+]
+ 
+ 
+app.get("/", (req, res, next)=>{
+    res.send("Hello World")
+})
+app.get("/boxes", (req, res, next)=>{
+    res.send(JSON.stringify(boxes))
+})
+ 
+app.listen(4321,()=>{
+    console.log("This is working")
+})
+ 
+ 
+console.log("hello")
