cd627c5d239f027388df84f07f2d62c0a2e12215
Purevsuren1 <113395417+Purevsuren1@users.noreply.github.com>
2024-01-22 14:43:44 -0700

    worked on backed - connecting to database

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-const express = require("express")
-const app = express();
- 
-const boxes = [
-    {boxId:1},
-    {boxId:2},
-    {boxId:3},
-    {boxId:4},
-]
- 
- 
-app.get("/", (req, res, next)=>{
-    res.send("Hello World")
-})
-app.get("/boxes", (req, res, next)=>{
-    res.send(JSON.stringify(boxes))
-})
- 
-app.listen(4321,()=>{
-    console.log("This is working")
-})
- 
+const express = require("express");
+
+ const Redis = require('redis'); // import the Redis library
+
+ const redisClient = Redis.createClient({
+    url:`redis://localhost:6379`
+
+ });
+
+
+const app = express(); //create a express application
+const port = 3000;
+app.listen(port, () =>{
+    redisClient.connect(); //connects to REdis database
+    console.log(`API is listening on port: ${port}`)//template literal
+}); //listen fopr browser from the front end
+
+
+//1 URL
+//2 a function to return boxes
+//3 req - request from the browser
+//res - response to the browser
+
+app.get('/boxes', (req, res, next)=>{
+    let boxes = redisClient.json.get('boxes', {path:'$'}); //get boxes
+    //send the boxes to browser
+    res.send(JSON.stringify(boxes)) //convert boxes into a string
+});
  
 console.log("hello")
