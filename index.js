ae5c9dfe9a0c4f32c8f4c1aeffd25bc2c7ebc5ac
Purevsuren1 <113395417+Purevsuren1@users.noreply.github.com>
2024-01-24 11:40:36 -0700

    added POST function to allow user to add data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
  const Redis = require('redis'); // import the Redis library
 
  const redisClient = Redis.createClient({
@@ -9,6 +11,7 @@ const express = require("express");
 
 
 const app = express(); //create a express application
+app.use(bodyParser.json());
 const port = 3000;
 app.listen(port, () =>{
     redisClient.connect(); //connects to REdis database
@@ -25,6 +28,16 @@ app.get('/boxes', (req, res, next)=>{
     let boxes = redisClient.json.get('boxes', {path:'$'}); //get boxes
     //send the boxes to browser
     res.send(JSON.stringify(boxes)) //convert boxes into a string
+});//return boxes to the user
+
+app.post('/boxes', async (req, res, next) => { //async means we will await promies
+    
+    const newBox = req.body; // Assuming you have body-parser middleware to parse JSON
+    newBox.id=parseInt(await redisClient.json.arrLen('boxes', '$'))+1; //user should not choose the ID
+    await redisClient.json.arrAppend('boxes', '$', newBox);
+    // saves boxes to REdis
+    res.json(newBox); //respond with the new box
 });
+
  
 console.log("hello")
